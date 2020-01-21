48c88a35ac940a74cff8208320c4d26e11598bda
Luc Comeau <comeaul@byui.edu>
2020-01-21 09:50:14 -0700

    Week 03

---
+++ week03/teach1.py
@@ -0,0 +1,96 @@
+"""
+Course: CS312
+File: teach1.py
+Description:
+   OpenCV functions
+
+   You are NOT submitting this program.
+"""
+
+"""
+Instructions:
+
+- You will impliementing the following functions using Opencv
+- The first step is to make sure that you have OpenCV for Python installed
+- You a free to use any images for the function below (except task 5)
+  - Download images from the Internet
+  - Use your own photos
+- You must download "usa.png" from the GitHub repo for task 5
+
+"""
+import numpy as np
+import cv2
+
+
+
+def task1():
+    """ task1: load and display an image """
+    print('Task 1')
+
+
+
+def task2():
+    """ Task2: Open image and flip left <-> right """
+
+    # 1) Try doing this task using loops and just the numpy array once you load the image
+
+    # 2) Use OpenCV function(s)
+
+    print('Task 2')
+
+
+
+def task3():
+    """ Task3: Open image and flip up <-> down """
+
+    # 1) Try doing this task using loops and just the numpy array once you load the image
+
+    # 2) Use OpenCV function(s)
+
+    print('Task 3')
+
+
+
+def task4():
+    """ Task4: Resample image down 50% """
+
+    # Do this task using loops and just the numpy array once you load the image
+
+    print('Task 4')
+
+
+
+def task5():
+    """ Task5: Tile a small image into a larger one """
+
+    # load 'usa.png' and create a large 400 x 600 tiled image
+
+    print('Task 5')
+
+
+
+def task6():
+    """ Task6: EXTRA - Tile a small image into a larger one like a brick wall """
+
+    # load 'usa.png' and create a large 400 x 600 tiled image
+
+    print('Task 6')
+
+
+
+def main():
+    """ Main function """
+
+    task1()
+    task2()
+    task3()
+    task4()
+    task5()
+    task6()
+
+
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
